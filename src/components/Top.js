130a2a5065c2d75f0cb0212d122f9fffb331c6e3
Swapy221 <127690806+Swapy221@users.noreply.github.com>
2023-07-30 19:09:43 +0530

    Update Top.js

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,22 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Top() {
-    const [cryptoArr, setCryptoArr] = useState([]);
+  const [cryptoArr, setCryptoArr] = useState([]);
 
-    return (
-        <div className='top-container'>
-            <h1>Top 10 Global Crypto Coins</h1>
-            <div className='display-container'>
+  useEffect(() => {
+    fetchTopCryptoData();
+  }, []);
 
-                <div className='coin-container'>
-                    <p className='rank-para'>Rank #rank</p>
-                    <h3 className='coin-symbol'>name (symbol)</h3>
-                    <p className='price-para'>Price: price_usd</p>
-                </div>
+  const fetchTopCryptoData = async () => {
+    try {
+      const response = await axios.get('https://api.coinlore.net/api/tickers/');
+      const topTenCrypto = response.data.data.slice(0, 10);
+      setCryptoArr(topTenCrypto);
+    } catch (error) {
+      console.error('Error fetching top crypto data:', error);
+    }
+  };
 
-            </div>
-        </div>
-    )
+  return (
+    <div className='top-container'>
+      <h1>Top 10 Global Crypto Coins</h1>
+      <div className='display-container'>
+        {cryptoArr.map((crypto) => (
+          <div className='coin-container' key={crypto.id}>
+            <p className='rank-para'>Rank #{crypto.rank}</p>
+            <h3 className='coin-symbol'>{crypto.name} ({crypto.symbol})</h3>
+            <p className='price-para'>Price: {crypto.price_usd}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Top
+export default Top;
